d843247b2fded351d5baffc3d9bd9938910c3f55
dodycs <dody@cometsoul.com>
2018-06-05 12:13:24 +0800

    redirect to index

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
-from flask import Flask, flash, session, request, redirect, render_template
+from flask import Flask, flash, session, request, redirect, render_template, url_for
 from db.data_layer import get_all_bills, get_bill, create_bill, delete_bill, update_bill
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    b = get_all_bills()
+    print('--------------------')
+    print(b)
+    return render_template('index.html', bills = b)
 
 @app.route('/add_bill', methods=['POST'])
 def add_bill():
     amount = request.form['amount']
     description = request.form['description']
     create_bill(amount, description)
-    b = get_all_bills()
-    return render_template('index.html', bills = b)
+
+    return redirect(url_for('index'))
 
 app.run(debug=True)
